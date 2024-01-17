e9272515603a1c2b1b5fdbf28047ea6a33e37be9
draquio <draquio@gmail.com>
2024-01-17 11:13:39 -0400

    suspnse

--- src/app/post/[postId]/page.jsx
+++ src/app/post/[postId]/page.jsx
@@ -1,3 +1,5 @@
+import Post from "../page";
+import { Suspense } from "react";
 const loadPost = async (id) => {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${id}`
@@ -8,12 +10,18 @@ const loadPost = async (id) => {
 const PostPage = async (props) => {
   const { params } = props;
   const postId = params.postId;
-  const post = await loadPost(postId)
+  const post = await loadPost(postId);
   return (
     <div>
       <h2>Esta es la id {postId}</h2>
       <h3>{post.title}</h3>
       <p>{post.body}</p>
+      <br />
+      <hr />
+      <Suspense fallback={<div>Cargando publicaciones</div>}>
+        <h2>Mira más post</h2>
+        <Post />
+      </Suspense>
     </div>
   );
 };
--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,10 +1,9 @@
 import PostCard from "@/components/postcard/PostCard";
-import { resolve } from "styled-jsx/css";
 
 const loadPost = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const results = await response.json();
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   return results;
 };
 
